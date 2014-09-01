564723195c0316bd11da6688bd3ed1e58e6bc942
thedpr <palampinen@gmail.com>
2014-09-01 09:18:44 +0300

    changed some style, removed striped tabs, new functionality: adding checks existing items preventing duplicates

--- js/controllers.js
+++ js/controllers.js
@@ -9,9 +9,30 @@ angular.module('shoplist.controllers', [])
   //  Add item, parameters text, added, done
   $scope.addItem = function(text) {
   	var added = new Date().getTime();
-	Items.$add({name:text, added: added, done:false}).then(function(data) {
-		$scope.itemText = ''
-	});
+
+    
+    // search if item exists, preventing duplicates
+    var i = 0,
+        t = text.toUpperCase(),
+        exists =  false;
+    Items.forEach(function(element) {
+      if(element.name.toUpperCase() == t){
+
+          Items[i].done = false,
+          Items[i].added = added;
+          Items.$save(Items[i]);
+          exists = true;
+          // end loop
+          return false;
+        }
+      i++;
+    });
+
+    // add item if not found
+    if(!exists)
+    	Items.$add({name:text, added: added, done:false}).then(function(data) {
+    		$scope.itemText = ''
+    	});
 
   };
 
